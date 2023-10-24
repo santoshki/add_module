dbd6153e43b1e8d7672271444dd80180619cf7c2
santoshki <santoshkdevp@gmail.com>
2023-10-24 15:42:09 +0530

    Packaged module.

---
+++ README.md
@@ -0,0 +1,1 @@
+use pip install add_module for installation and import the ad_module package.
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="add_module",
+    version="0.1",
+    description="Python package for adding of 2 integer variables.",
+    author="Santosh K",
+    author_email="santoshkdevp@gmail.com",
+    github_url="https://github.com/santoshki/add_module",
+    packages=["add_module"],
+)
